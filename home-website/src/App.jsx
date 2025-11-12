f5733a8e4048557ded81e240b11315cb32b825d2
Michael Burton <michael.v.burton@gmail.com>
2025-11-12 17:14:40 -0500

    boiler plate and initial entries

---
+++ home-website/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Banner from "./Banner/Banner";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ home-website/src/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css";
+import { motion } from "framer-motion";
+
+const Banner = () => {
+  return (
+    <>
+      <motion.div className="banner-container">
+        <div className="banner-text">Michael Burton</div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Banner;
