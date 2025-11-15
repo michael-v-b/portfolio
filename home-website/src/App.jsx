c2228a324b660dbe24775c7f5e9871a81b684ac4
Michael Burton <michael.v.burton@gmail.com>
2025-11-15 14:56:07 -0500

    add down arrow and readjust banner

--- home-website/src/App.jsx
+++ home-website/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "./Banner/Banner";
 import Background from "./Background/Background";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import DownArrow from "./assets/down-arrow.svg?react";
+import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
@@ -10,6 +10,22 @@ function App() {
     <>
       <Background />
       <div className="web-container">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, y: [0, 10, 0] }}
+          transition={{
+            opacity: { delay: 3 },
+            y: {
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "easeInOut",
+            },
+          }}
+        >
+          <DownArrow className="down-arrow" />
+        </motion.div>
+        <div className="about-container">This is full of text</div>
         <Banner />
       </div>
     </>
